6aaf760287c06baef306beebe4a07350a3ec6eb3
Liam Ramirez <92497517+liamramirez02@users.noreply.github.com>
2023-04-08 01:14:11 -0400

    Validacion del usuario en favoritos
    Validacion para ver si el usuario esta logueado para ver su mascotas favoritas

--- screens/Favoritos.js
+++ screens/Favoritos.js
@@ -1,27 +1,86 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, {useState,useCallback} from 'react'
+import React, {useState,useCallback,useRef} from 'react'
 import { useFocusEffect } from '@react-navigation/native';
 import { getFavoritelist } from './../utilidades/actions';
+import { Image, Icon, Button } from 'react-native-elements'
+import Toast from 'react-native-easy-toast'
+import Loading from './../Componentes/Loading';
+import firebase from "firebase/app"
+import "firebase/firestore"
 
-export default function Favoritos() {
 
+
+export default function Favoritos({navigation}) {
+
+  const toastRef = useRef()
+
+  const [mascotas, setMascotas] = useState(null)
+  const [userLogged, setUserLogged] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [reloadData, setReloadData] = useState(false)
+
+  firebase.auth().onAuthStateChanged((user) => {
+    user ? setUserLogged(true) : setUserLogged(false)
+})
 
   useFocusEffect(
     useCallback(() => {
-      async function getData() {
-        const response = await getFavoritelist()
-        console.log(response)
-      }
-      
-    getData()
-    },[])
+      if (userLogged){  
+        async function getData() {
+          setLoading(true)
+          const response = await getFavoritelist()
+          setMascotas(response.favorites)
+          setLoading(false)     
+      } 
+      getData()
+    }
+    setReloadData(false)
+    },[userLogged, reloadData])
 )
+
+if (!userLogged) {
+  return <UserNoLogged navigation={navigation}/>
+}
+
+if (!mascotas) { //si no hay valor, aun siguen cargando las mascotas.  Si no hay mascotas favoritas llama a "NotFoundRestaurants"
+  return <Loading isVisible={true} text="Cargando Mascotas..."/>
+} else if(mascotas?.length === 0) {
+  return <NotFoundMascotas/>
+}
 
   return (
     <View>
       <Text></Text>
+      <Toast ref={toastRef} position="center" opacity={0.9}/>
+      <Loading text="Eliminando mascota..." isVisible={Loading}/>
     </View>
   )
 }
 
+function NotFoundMascotas() {
+  return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <Icon type="material-community" name="alert-outline" size={50}/>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Aún no tienes Mascotas favoritas</Text>
+      </View>
+  )    
+}
+
+function UserNoLogged({navigation}) {
+
+  return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <Icon type="material-community" name="alert-outline" size={50}/>
+          <Text style={{ fontSize: 20, fontWeight: "bold", textAlign: "center" }}>
+              Necesitas estar logueado para ver los favoritos
+          </Text>
+          <Button 
+              title="Ir al Login" 
+              containerStyle={{ marginTop: 20, width: "80%"}}
+              buttonStyle={{ backgroundColor: "#442484" }}
+              onPress={() => navigation.navigate("C2", { screen: "login" })}
+          />
+      </View>
+  )
+}
 const styles = StyleSheet.create({})
